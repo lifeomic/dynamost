06af5707b57eebd5c04eb4844fca934a781fda99
Alejandro Corredor <alejandro.corredor@lifeomic.com>
2023-08-28 15:35:40 -0400

    enforce coverage

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,12 @@ module.exports = {
   preset: 'ts-jest',
   clearMocks: true,
   collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      statements: 66,
+      branches: 36,
+      lines: 66,
+      functions: 56,
+    },
+  },
 };
